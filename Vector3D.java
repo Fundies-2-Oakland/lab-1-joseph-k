cad70e8aaab3828fce5f412565107c34b8ebc127
Joseph Kelly <josephkelly1107@gmail.com>
2025-01-10 13:25:40 -0800

    steps 6, 7, 8, 9

--- Vector3D.java
+++ Vector3D.java
@@ -33,11 +33,30 @@ public class Vector3D{
     public Vector3D normalize() {
         double magnitude = getMagnitude();
         if (magnitude == 0.0) {
-            throw new IllegalStateException("Vector3D normalize() called with magnitude of 0");
+            throw new IllegalStateException("Can't call normalize() with magnitude of 0");
         }
         x /= magnitude;
         y /= magnitude;
         z /= magnitude;
         return this;
+    }
+
+    public Vector3D add(Vector3D v) {
+        return new Vector3D(x + v.getX(), y + v.getY(), z + v.getZ());
+    }
+
+    public Vector3D multiply(double scalar) {
+        return new Vector3D(x * scalar, y * scalar, z * scalar);
+    }
+
+    public double dotProduct(Vector3D v) {
+        return x * v.getX() + y * v.getY() + z * v.getZ();
+    }
+
+    public double angleBetween(Vector3D v) {
+        if (getMagnitude() == 0.0 || v.getMagnitude() == 0.0) {
+            throw new IllegalStateException("Can't call angleBetween() with magnitude of 0");
+        }
+        return Math.acos(dotProduct(v) / (getMagnitude() * v.getMagnitude()));
     }
 }
