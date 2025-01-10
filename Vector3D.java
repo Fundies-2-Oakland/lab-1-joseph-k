ebcd6491400eb92a9fb8ed65953f954f1d2b4396
Joseph Kelly <josephkelly1107@gmail.com>
2025-01-10 13:01:42 -0800

    steps 1 and 2

--- Vector3D.java
+++ Vector3D.java
@@ -1,2 +1,25 @@
-public class Vector3D {
+public class Vector3D{
+
+    double x, y, z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+
+
+    }
+
+    public double getX() {
+            return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getZ() {
+        return z;
+    }
+
+
 }
